2fdf050aad012e4f533f5e82e724cfa6c791fa6e
aless <aless@DESKTOP-C2P91P6>
2019-03-18 13:56:20 +0100

    punto 2

--- src/it/polito/tdp/libretto/Libretto.java
+++ src/it/polito/tdp/libretto/Libretto.java
@@ -22,4 +22,20 @@ public class Libretto {
 	public void add(Voto v) { //più efficiente perchè se cambia struttura voto qui rimane invariato.
 		voti.add(v);
 	}
+	
+	/**
+	 * Seleziona il sottoinsieme di voti che hanno il punteggio specificato
+	 * 
+	 * @param punti punteggio da ricerca
+	 * @return lista di {@link Voto} aventi quel punteggio
+	 */
+	public List<Voto> cercaVoti(int punti){
+		List<Voto> result = new ArrayList<Voto>();
+		for(Voto v:this.voti) {
+			if(v.getPunti()==punti) {
+				result.add(v);
+			}
+		}
+		return result;
+	}
 }
--- src/it/polito/tdp/libretto/TestLibretto.java
+++ src/it/polito/tdp/libretto/TestLibretto.java
@@ -1,6 +1,7 @@
 package it.polito.tdp.libretto;
 
 import java.time.LocalDate;
+import java.util.*;
 
 public class TestLibretto {
 
@@ -8,7 +9,7 @@ public class TestLibretto {
 
 		Libretto libr = new Libretto();
 		libr.add(new Voto(30,"Analisi 1",LocalDate.of(2017, 1, 15)));
-		libr.add(new Voto(21,"Fisica 1",LocalDate.of(2018, 2, 25)));
+		libr.add(new Voto(25,"Fisica 1",LocalDate.of(2018, 2, 25)));
 		libr.add(new Voto(30,"Geome 1",LocalDate.of(2017, 1, 15)));
 		libr.add(new Voto(28,"Analisi 2",LocalDate.of(2018, 2, 15)));
 		libr.add(new Voto(26,"Fisica 2",LocalDate.of(2017, 1, 15)));
@@ -16,6 +17,9 @@ public class TestLibretto {
 		libr.add(new Voto(29,"po",LocalDate.of(2017, 1, 15)));
 		libr.add(new Voto(28,"Ric Op",LocalDate.of(2017, 1, 15)));
 		libr.add(new Voto(30,"pwr",LocalDate.of(2019, 6, 15)));
+		
+		List<Voto> venticinque = libr.cercaVoti(25);
+		System.out.println(venticinque);
 		
 		
 		
